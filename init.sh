ca677c3a5b2b54d22db39daed2ddccdbc1a8487d
Black Dragon(白い巨匠) <48268998+bzgwhite@users.noreply.github.com>
2020-09-03 13:59:35 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-current_dir=`pwd`
 
 docker build -t breezeshared/python_training . && \
-id=`docker images breezeshared/python_training -q` && \
 docker run --name python_training \
   --detach -p 5678:5678 \
-  -itv ${current_dir}/work:/python_training ${id} /bin/bash && \
+  -itv work:/python_training breezeshared/python_training /bin/bash && \
 docker start python_training && \
 docker exec -it python_training git clone https://github.com/breeze-shared-inc/python_training_01.git /python_training
