b59f728e3ba155367c98baad5426210bc25300aa
Black Dragon(白い巨匠) <48268998+bzgwhite@users.noreply.github.com>
2020-09-03 19:40:58 +0900

    create init.bat for windows

---
+++ init.bat
@@ -0,0 +1,6 @@
+docker build -t breezeshared/python_training . && ^
+docker run --name python_training ^
+  --detach -p 5678:5678 ^
+  -itv work:/python_training breezeshared/python_training /bin/bash && ^
+docker start python_training && ^
+docker exec -it python_training git clone https://github.com/breeze-shared-inc/python_training_01.git /python_training
